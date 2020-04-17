9258514ee195c792657553d49085b2f68bd2934e
ËﺣÁÁ <sunhl@Fosun.com>
2020-04-17 17:22:51 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <ul>
+      <li v-for="item in users"
+          :key="item.id">
+        {{item}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios'
+export default {
+  name: 'App',
+  data () {
+    return {
+      users: ''
+    }
+  },
+  mounted () {
+    axios.get("http://localhost:3001/api/users").then(res => {
+      this.users = res.data
+    })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
